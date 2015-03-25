ee916cb8fde5a50f404db2c3d72855cb6551c449
Richard Jansen <richard@lrtbl.nl>
2015-03-25 11:54:04 +0100

    done

--- opdracht-4.py
+++ opdracht-4.py
@@ -1,10 +1,11 @@
 ###############################################################################
-##Afvinkopdracht 3, Blok 3, Richard Jansen HAN University Of Applied Sciences##
-##17-03-2015###################################################################
+##Afvinkopdracht 4, Blok 3, Richard Jansen HAN University Of Applied Sciences##
+##24-03-2015###################################################################
 ###############################################################################
 
 from tkinter import *
 from tkinter import filedialog
+import re
 niets=Tk()
 file = filedialog.askopenfile()
 niets.destroy()
@@ -13,7 +14,8 @@ niets.mainloop()
 def main():
     x = startread(file)
     checkaa(x)
-   
+    matching(x)
+    
 def startread(seq):
     raw_data = ""
     startReading = False
@@ -29,8 +31,15 @@ def startread(seq):
 def checkaa(x):
     seq = x.upper()
     if "B" in seq:
-        print ('character B found in sequence, likely not a proteinsequence')
+        print ('\ncharacter B found in sequence, likely not a proteinsequence')
     else:
         print ('\nall characters are possibly amino-acids')
 
+def matching(x):
+    result = re.match('(MCNSSC)[MV](GGMNRR)', x)
+    if result:
+        print ('\npattern not found')
+    else:
+        print ('\npattern found')
+      
 main()
